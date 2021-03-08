0fceef6fbdaad9b40cf7f2eedeee149f95b46c2d
Philip Lehmann <philiplehmann@gmail.com>
2021-03-08 21:17:29 +0100

    update pdf api and use print on index

--- pages/api/pdf.js
+++ pages/api/pdf.js
@@ -1,6 +1,35 @@
-/* global process */
+/* global process, __dirname */
 
 import request from 'request'
+import fs from 'fs'
+import path from 'path'
+
+const outputPath = path.resolve('cv_pdf.pdf')
+console.log(__dirname, outputPath)
+
+const getFile = () => {
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(outputPath)) {
+      resolve(fs.createReadStream(outputPath))
+    } else {
+      request
+        .post(opts)
+        .on('error', (err) => {
+          reject(new Error(err))
+        })
+        .on('response', (response) => {
+          if (response.statusCode === 200) {
+            response.pipe(fs.createWriteStream(outputPath)).on('finish', () => {
+              console.log('PDF saved to disk')
+              resolve(getFile())
+            })
+          } else {
+            reject(new Error(response))
+          }
+        })
+    }
+  })
+}
 
 const opts = Object.freeze({
   uri: 'https://api.sejda.com/v2/html-pdf',
@@ -13,23 +42,19 @@ const opts = Object.freeze({
   }
 })
 
-export default function handler(req, res) {
+export default async (req, res) => {
   // res.status(200).json({ name: 'John Doe' })
-  request
-    .post(opts)
-    .on('error', function (err) {
-      res.status(500).json(err)
-      console.error(err)
+  try {
+    const response = await getFile()
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+      'Content-Disposition': 'attachment; filename="cv.pdf'
     })
-    .on('response', function (response) {
-      if (response.statusCode === 200) {
-        response.pipe(res).on('finish', () => {
-          res.end()
-          console.log('PDF send')
-        })
-      } else {
-        console.error('Got code: ' + response.statusCode)
-        res.status(500).json(response)
-      }
+    response.pipe(res).on('finish', () => {
+      res.end()
+      console.log('PDF send')
     })
+  } catch (e) {
+    res.status(500).json(e)
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import React, { useEffect } from 'react'
 
 import { Container } from '@bootstrap-styled/v4'
+import styled from 'styled-components'
 import { Reset } from 'styled-reset'
 
 import CV from 'components/cv'
 import Layout from 'components/layout'
 import hireMe from 'helpers/hire_me'
+
+const PrintContainer = styled(Container)`
+  max-width: calc(100vw - 30px);
+`
 
 const IndexPage = () => {
   useEffect(hireMe)
@@ -13,9 +18,9 @@ const IndexPage = () => {
     <>
       <Reset />
       <Layout>
-        <Container>
+        <PrintContainer>
           <CV />
-        </Container>
+        </PrintContainer>
       </Layout>
     </>
   )
