8adf00fcc0f690129da66fd4b1c183a6dcc54512
Philip Lehmann <philiplehmann@gmail.com>
2025-02-17 00:20:10 +0100

    Add TypeScript and Ruby on Rails translations and refactor ComputerLanguages component to use Stack for improved layout

--- src/components/sidebar/computer_languages.tsx
+++ src/components/sidebar/computer_languages.tsx
@@ -1,16 +1,19 @@
 import type { FC } from 'react';
-
 import { useTranslations } from 'next-intl';
+import Stack from '@mui/material/Stack';
 import { SidebarItem } from './helper';
+
+const LANGUAGES = ['typescript', 'rubyOnRails'] as const;
 
 export const ComputerLanguages: FC = () => {
   const t = useTranslations('ComputerLanguages');
-
   return (
     <SidebarItem title={t('name')}>
-      Typescript
-      <br />
-      Ruby on Rails
+      <Stack spacing={1}>
+        {LANGUAGES.map((lang) => (
+          <div key={lang}>{t(lang)}</div>
+        ))}
+      </Stack>
     </SidebarItem>
   );
 };
