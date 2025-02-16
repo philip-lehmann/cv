33127caeb55f6f939f7af0147c9870e618c7cc7e
Philip Lehmann <philiplehmann@gmail.com>
2025-02-17 00:16:47 +0100

    Refactor HumanLanguages component to use Stack for better layout and simplify language rendering

--- src/components/sidebar/human_languages.tsx
+++ src/components/sidebar/human_languages.tsx
@@ -1,17 +1,19 @@
 import type { FC } from 'react';
 import { useTranslations } from 'next-intl';
+import Stack from '@mui/material/Stack';
 import { SidebarItem } from './helper';
+
+const LANGUAGES = ['swiss', 'german', 'english'] as const;
 
 export const HumanLanguages: FC = () => {
   const t = useTranslations('HumanLanguages');
-
   return (
     <SidebarItem title={t('name')}>
-      {t('swiss')}
-      <br />
-      {t('german')}
-      <br />
-      {t('english')}
+      <Stack spacing={1}>
+        {LANGUAGES.map((lang) => (
+          <div key={lang}>{t(lang)}</div>
+        ))}
+      </Stack>
     </SidebarItem>
   );
 };
