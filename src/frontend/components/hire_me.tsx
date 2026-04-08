ff032a65c01da9c8dc4970aa00bef4950e7b366b
Philip Lehmann <philiplehmann@gmail.com>
2026-04-08 23:35:07 +0200

    Remove debug console.log from HireMe component

--- src/frontend/components/hire_me.tsx
+++ src/frontend/components/hire_me.tsx
@@ -4,7 +4,6 @@ import { type FC, useEffect } from 'react';
 import { hireMe } from '../../helpers/hire_me';
 
 export const HireMe: FC = () => {
-  console.log('hireme');
   useEffect(hireMe, []);
   return null;
 };
